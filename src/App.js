a05ce0d3374e79795e851c48f1f8a21ba0dd7752
Ulan <ulan97kgma@gmail.com>
2021-08-20 15:44:28 +0600

    cart layout styling done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2 className="mb-30">Корзина</h2>
+          <h2 className="mb-30 d-flex justify-between">
+            Корзина
+            <img
+              className="removeBtn cu-p"
+              src="/img/btn-remove.svg"
+              alt="Remove"
+            />
+          </h2>
 
           <div className="items">
             <div className="cartItem d-flex align-center mb-20">
@@ -37,20 +44,40 @@ function App() {
                 alt="Remove"
               />
             </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20">
+                <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
+                <b>12 999 сом</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
           </div>
 
-          <ul className="cartTotalBlock">
-            <li>
-              <span>Итого:</span>
-              <div></div>
-              <b>21 498 сом </b>
-            </li>
-            <li>
-              <span>Налог 5%:</span>
-              <div></div>
-              <b>1074 сом</b>
-            </li>
-          </ul>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 сом </b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 сом</b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
+            </button>
+          </div>
         </div>
       </div>
       <header className="d-flex justify-between align-center p-40">
